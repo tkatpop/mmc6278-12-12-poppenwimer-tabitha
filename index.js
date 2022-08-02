e2c489dc739b4370ef1afa4c8da4c898396ae408
tkatpop <93983050+tkatpop@users.noreply.github.com>
2022-08-02 20:07:43 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const words = [
+  'bananas',
+  'grapes',
+  'carousel',
+  'milkshake',
+  'javascript',
+  'limousine',
+  'chocolate',
+  'programming',
+  'meatloaf',
+  'ukulele',
+  'mango'
+]
+
+let wins = 0
+let losses = 0
+let currentWord
+
+class Word {
+  constructor(word) {
+    this.word = word
+    this.displayWord = word.replaceAll(/[\w]/g, "_")
+    this.remainingGuesses = 10
+    this.incorrectLetters = []
+    this.correctLetters = []
+  }
+
+  // implement the guessLetter function:
+  // guessLetter(letter) {}
+
+  // implement the updateScreen function:
+  // updateScreen() {}
+
+  // implement the isGameOver function:
+  // isGameOver() {}
+
+  // implement the getWinOrLoss function:
+  // getWinOrLoss() {}
+}
+
+function newGame() {
+  const randomWord = words[Math.floor(Math.random() * words.length)]
+  currentWord = new Word(randomWord)
+  currentWord.updateScreen()
+}
+
+document.onkeyup = function(e) {
+  const pressedKey = e.key.toLowerCase()
+  // early exit for non-letter key presses
+  if (!/^[a-z]{1}$/g.test(pressedKey)) return
+
+  // pass in guessed letter to word obj
+  currentWord.guessLetter(pressedKey)
+  // allow word obj to update screen
+  currentWord.updateScreen()
+
+  // check if game is over
+  const gameOver = currentWord.isGameOver()
+
+  // if game is over, update wins/losses and start new game
+  if (gameOver) {
+    const previousWord = document.getElementById('previous-word')
+    const winDisplay = document.getElementById('wins')
+    const lossDisplay = document.getElementById('losses')
+    previousWord.textContent = currentWord.word
+    const result = currentWord.getWinOrLoss()
+    if (result === 'win') {
+      wins++
+      winDisplay.textContent = wins
+    } else if (result === 'loss') {
+      losses++
+      lossDisplay.textContent = losses
+    }
+    newGame()
+  }
+}
+
+newGame()
