07e826fc1cb040d6a36461436c094df09c72855d
tkatpop <93983050+tkatpop@users.noreply.github.com>
2022-08-04 09:54:52 +0300

    guessLetter function started, displayWord showing

--- index.js
+++ index.js
@@ -24,18 +24,88 @@ class Word {
     this.incorrectLetters = []
     this.correctLetters = []
   }
-
+ ///////////////////////////////////////////
   // implement the guessLetter function:
-  // guessLetter(letter) {}
+  guessLetter(letter) {
+    console.log(letter)
+    console.log(this.word)
+    console.log(this.word.indexOf(letter))
+    //Check if letter is in the word
+    if (this.word.indexOf(letter) > -1){ //Letter is in word, value > -1
+      
+      //updated word
+      var updated_word = []
+
+      //Check each letter to see if it matches the keyed letter
+      for (let i = 0; i < this.word.length; i++) {
+        
+        //If letter in word matches letter
+        if (this.word[i] == letter){ //Current letter matches guessed letter
+
+          //Add guessed letter
+          updated_word.push(letter)
+        } else { //Current letter is NOT guessed letter
+
+          //Add this.displayword letter
+          updated_word.push(this.displayWord[i])
+        }
+        
+      }
+      
+      //update displayWord
+      this.displayWord = updated_word.join("")
+      console.log(this.displayWord)
+
+      //Add to correct letter []
+      this.correctLetters.push(letter)
+      console.log(this.correctLetters)
+
+
+    }else { //Letter is NOT in word
+      console.log("NOT in word")
+    }
+    // if (letter = true ) {
+    //   this.displayWord
+    //   this.correctLetters
+    // } else {
+    //   this.incorrectLetters
+    //   (this.remainingGuesses - 1)
+    // }
+  }
 
   // implement the updateScreen function:
-  // updateScreen() {}
+  updateScreen() {
 
-  // implement the isGameOver function:
-  // isGameOver() {}
+    //retrieved Elements by Ids
+    const remainingGuessesEl = document.getElementById("remaining-guesses")
+    const incorrectLettersEl = document.getElementById("incorrect-letters")
+    const displayWordEl = document.getElementById("word-to-guess")
 
-  // implement the getWinOrLoss function:
-  // getWinOrLoss() {}
+    //Assigned values to innerHTML
+    remainingGuessesEl.innerHTML = this.remainingGuesses
+    incorrectLettersEl.innerHTML = this.incorrectLetters
+    displayWordEl.innerHTML = this.displayWord
+  }
+
+  // // implement the isGameOver function:
+  // isGameOver() {
+  //   if (this.remainingGuesses.value <= 0 | word == this.displayWord) {
+  //     true
+  //   } else {
+  //     false
+  //   }
+  // }
+
+  // // implement the getWinOrLoss function:
+  // getWinOrLoss() {
+  //   if (word == this.displayWord & this.remainingGuesses.value > 0){
+  //     "win"
+  //   } else (this.displayWord != word & this.remainingGuesses <= 0) {
+  //     "loss"
+  //   } return null 
+  // }
+  ///////////////////////////////////////////////////
+
 }
 
 function newGame() {
@@ -45,6 +115,7 @@ function newGame() {
 }
 
 document.onkeyup = function(e) {
+  console.log("duck")
   const pressedKey = e.key.toLowerCase()
   // early exit for non-letter key presses
   if (!/^[a-z]{1}$/g.test(pressedKey)) return
